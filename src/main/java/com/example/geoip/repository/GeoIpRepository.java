45b410ed8ba4747e946757bd2d113c00e2e300de
Mykyta Semisoshenko <msemisoshenko@gmail.com>
2022-09-03 11:10:31 +0200

    checkstyle was added

--- src/main/java/com/example/geoip/repository/GeoIpRepository.java
+++ src/main/java/com/example/geoip/repository/GeoIpRepository.java
@@ -2,14 +2,9 @@ package com.example.geoip.repository;
 
 import com.example.geoip.model.GeoIp;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GeoIpRepository extends JpaRepository<GeoIp, Long> {
-//    @Query(value = "from GeoIp g where ?1 between g.start and g.end")
-//    GeoIp findGeoIp(Long ipAsDecimal);
     GeoIp findGeoIpByStartIsLessThanEqualAndEndIsGreaterThanEqual(Long ipAsDecimal, Long ipRepeat);
-
-
 }
--- src/main/java/com/example/geoip/service/GeoIpServiceImpl.java
+++ src/main/java/com/example/geoip/service/GeoIpServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.geoip.service;
 
-import java.util.Optional;
 import com.example.geoip.model.GeoIp;
 import com.example.geoip.repository.GeoIpRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +12,8 @@ public class GeoIpServiceImpl implements GeoIpService {
 
     @Override
     public GeoIp findIp(Long ipAsDecimal) {
-        return geoIpRepository.findGeoIpByStartIsLessThanEqualAndEndIsGreaterThanEqual(ipAsDecimal, ipAsDecimal);
+        return geoIpRepository.findGeoIpByStartIsLessThanEqualAndEndIsGreaterThanEqual(
+                ipAsDecimal,
+                ipAsDecimal);
     }
 }
